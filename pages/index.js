57aa237a8be142e6e68dd7e51f9feb482957ab90
nyg <nyg@users.noreply.github.com>
2021-04-05 19:58:07 +0200

    Improve tooltip, legend and chart style

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,8 @@ const assetsInfo = {
   }
 }
 
+const formatter = new Intl.DateTimeFormat('en-GB', { month: 'numeric', day: 'numeric' })
+const longDateFormatter = new Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'long', day: 'numeric' })
 
 export default function Home() {
 
@@ -26,10 +28,10 @@ export default function Home() {
   const CustomTooltip = ({ active, payload, label }) => {
     return (
       <div className="grid grid-cols-2 bg-gray-100 bg-opacity-90 border-2 border-gray-500 rounded-md p-3 text-sm">
-        <div className="col-span-2 text-center border-b-2 border-gray-400 mb-2">{label}</div>
-        {payload.map(y => <>
-          <div className="pr-3 ">{y.dataKey}</div>
-          <div className="text-right tabular-nums">{y.value.toFixed(2)}</div>
+        <div className="col-span-2 text-center border-b-2 border-gray-400 mb-2">{longDateFormatter.format(label)}</div>
+        {payload.sort((a, b) => b.value - a.value).map(y => <>
+          <div key={y.dataKey} className="pr-3 ">{y.dataKey}</div>
+          <div key={y.dataKey + 'val'} className="text-right tabular-nums">{y.value.toFixed(2)}</div>
         </>)}
       </div>
     )
@@ -45,15 +47,15 @@ export default function Home() {
   else {
     graph = (
       <ResponsiveContainer width="100%" aspect={1.618}>
-        <LineChart data={data.yields} margin={{ top: 20, right: 70, bottom: 0, left: 0 }}>
+        <LineChart data={data.yields} margin={{ top: 0, right: 3, bottom: 0, left: 10 }}>
           {data.assets.map(asset => (
             <Line key={asset} type="stepAfter" dataKey={asset} stroke={assetsInfo[asset].color} strokeWidth={2} dot={false} unit="%" />
           ))}
           <CartesianGrid stroke="#ddd" strokeDasharray="3 3" />
-          <XAxis dataKey="date" scale="time" type="number" interval={7} domain={['auto', 'auto']}  tickFormatter={(timestamp) => new Intl.DateTimeFormat('en-GB', { month: 'numeric', day: 'numeric' }).format(new Date(timestamp))} />
-          <YAxis unit="%" />
-          <Tooltip itemSorter={e => -e.value} content={<CustomTooltip />} />
-          <Legend iconType="plainline" onClick={function (e) { console.log(e) }} />
+          <XAxis tickMargin={10} dataKey="date" scale="time" type="number" interval={7} domain={['auto', 'auto']} tickFormatter={(timestamp) => formatter.format(timestamp)} />
+          <YAxis tickMargin={10} unit="%" />
+          <Tooltip content={<CustomTooltip />} />
+          <Legend iconType="plainline" align="center" wrapperStyle={{ paddingLeft: "52px", paddingTop: "20px" }} />
         </LineChart>
       </ResponsiveContainer>
     )
@@ -69,7 +71,7 @@ export default function Home() {
 
       <main className="flex flex-col h-screen text-gray-600">
 
-        <header className="p-10">
+        <header className="pt-10 pb-10 pl-16">
           <h1 className="text-4xl text-gray-800 font-bold">Yield Borg</h1>
           <p className="text-sm">Chart showing the evolution of the different <a href="http://swissborg.com/smart-yield-account">SwissBorg</a> Smart Yields</p>
         </header>
