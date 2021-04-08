7863e54bbd2be2825a23b407e96e5af60724323d
nyg <nyg@users.noreply.github.com>
2021-04-08 22:40:27 +0200

    Improve layout and style

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const assetsInfo = {
 }
 
 const formatter = new Intl.DateTimeFormat('en-GB', { month: 'numeric', day: 'numeric' })
-const longDateFormatter = new Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'long', day: 'numeric' })
+const longDateFormatter = new Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'short', day: 'numeric' })
 
 export default function Home() {
 
@@ -31,7 +31,7 @@ export default function Home() {
         <div className="col-span-2 text-center border-b-2 border-gray-400 mb-2">{longDateFormatter.format(label)}</div>
         {payload.sort((a, b) => b.value - a.value).map(y => <>
           <div key={y.dataKey} className="pr-3 ">{y.dataKey}</div>
-          <div key={y.dataKey + 'val'} className="text-right tabular-nums">{y.value.toFixed(2)}</div>
+          <div key={y.dataKey + 'val'} className="text-right tabular-nums">{y.value.toFixed(2)}%</div>
         </>)}
       </div>
     )
@@ -47,15 +47,15 @@ export default function Home() {
   else {
     graph = (
       <ResponsiveContainer width="100%" aspect={1.618}>
-        <LineChart data={data.yields} margin={{ top: 0, right: 3, bottom: 0, left: 10 }}>
+        <LineChart data={data.yields} margin={{ top: 40, right: 60, bottom: 20, left: 0 }}>
           {data.assets.map(asset => (
             <Line key={asset} type="stepAfter" dataKey={asset} stroke={assetsInfo[asset].color} strokeWidth={2} dot={false} unit="%" />
           ))}
           <CartesianGrid stroke="#ddd" strokeDasharray="3 3" />
-          <XAxis tickMargin={10} dataKey="date" scale="time" type="number" interval={7} domain={['auto', 'auto']} tickFormatter={(timestamp) => formatter.format(timestamp)} />
+          <XAxis tickMargin={10} dataKey="date" scale="time" type="number" interval={14} domain={['auto', 'auto']} tickFormatter={(timestamp) => formatter.format(timestamp)} />
           <YAxis tickMargin={10} unit="%" />
           <Tooltip content={<CustomTooltip />} />
-          <Legend iconType="plainline" align="center" wrapperStyle={{ paddingLeft: "52px", paddingTop: "20px" }} />
+          <Legend iconType="plainline" align="center" wrapperStyle={{ paddingLeft: "52px", paddingTop: "18px" }} />
         </LineChart>
       </ResponsiveContainer>
     )
@@ -67,26 +67,39 @@ export default function Home() {
         <title>Yield Borg</title>
         <link rel="icon" href="/favicon.ico" />
         <script data-goatcounter="/api/count" async src="/count.js"></script>
+        <meta name="viewport" content="viewport-fit=cover"/>
       </Head>
 
       <main className="flex flex-col h-screen text-gray-600">
 
-        <header className="pt-10 pb-10 pl-16">
-          <h1 className="text-4xl text-gray-800 font-bold">Yield Borg</h1>
-          <p className="text-sm">Chart showing the evolution of the different <a href="http://swissborg.com/smart-yield-account">SwissBorg</a> Smart Yields</p>
+        <header className="bg-gray-100">
+          <div className="lg:max-w-4xl pt-10 pb-10 pl-16">
+            <h1 className="text-4xl text-gray-800 font-bold">Yield Borg</h1>
+            <p className="text-sm">Chart showing the evolution of the different <a href="http://swissborg.com/smart-yield-account">SwissBorg</a> Smart Yields</p>
+          </div>
         </header>
 
+        {/* <nav className="bg-gray-200">
+          <div className="lg:max-w-4xl pt-4 pb-4 text-center text-sm">
+            Smart Yields
+          </div>
+        </nav> */}
+
         <div className="text-sm">
-          {graph}
+          <div className="lg:max-w-4xl lg:pl-2 lg:pr-2">
+            {graph}
+          </div>
         </div>
 
-        <footer className="mt-auto pt-3 pb-3 text-center text-sm text-gray-500 space-x-6">
-          <a href="https://github.com/nyg/yield-borg">
-            <Image src="/gh-dark.png" alt="github" width="24" height="24" />
-          </a>
-          <a href="https://yield-borg.goatcounter.com">
-            <Image src="/goatcounter.png" alt="goatcounter" width="24" height="24" />
-          </a>
+        <footer className="bg-gray-100 mt-auto">
+          <div className="lg:max-w-4xl space-x-6 pt-6 pb-3 text-center">
+            <a href="https://github.com/nyg/yield-borg">
+              <Image src="/gh-dark.png" alt="github" width="24" height="24" />
+            </a>
+            <a href="https://yield-borg.goatcounter.com">
+              <Image src="/goatcounter.png" alt="goatcounter" width="24" height="24" />
+            </a>
+          </div>
         </footer>
       </main>
     </div>
