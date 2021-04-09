b98ee99a29733cb6e32ca596695020542233db88
nyg <nyg@users.noreply.github.com>
2021-04-10 00:02:54 +0200

    Prepare db for community index scores

--- db/export-db.js
+++ db/export-db.js
@@ -7,12 +7,16 @@ const Redis = require('ioredis');
   const data = await fs.readFile('.env.development.local', 'ascii')
   const redisUrl = data.split('\n').filter(l => l.match(/^REDIS_URL=/))[0].split('=')[1]
 
-  // get all yields
+  // get all yields and community indices
   const redis = new Redis(redisUrl)
   const yields = await redis.lrange('yields', 0, -1)
+  const communityIndices = await redis.lrange('communityIndices', 0, -1)
   redis.quit()
 
   // save yields to file
-  await fs.writeFile('db/yields.json', JSON.stringify(yields))
+  await fs.writeFile('db/export.json', JSON.stringify({
+    yields: yields, communityIndices: communityIndices
+  }))
+
   console.log('Export done.')
 })()
--- db/import-db.js
+++ db/import-db.js
@@ -7,12 +7,18 @@ const Redis = require('ioredis');
   const data = await fs.readFile('.env.development.local', 'ascii')
   const redisUrl = data.split('\n').filter(l => l.match(/^REDIS_URL=/))[0].split('=')[1]
 
-  // read yields from file
-  const yields = JSON.parse(await fs.readFile('db/yields.json'))
+  // read yields and community indices from file
+  const { yields, communityIndices } = JSON.parse(await fs.readFile('db/export.json'))
 
-  // import yields into db
+  // import values into db
   const redis = new Redis(redisUrl)
+
+  await redis.del('yields')
   await redis.rpush('yields', yields)
+
+  await redis.del('communityIndices')
+  await redis.rpush('communityIndices', communityIndices)
+
   redis.quit()
 
   console.log('Import done.')
