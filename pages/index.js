4b0be94ec88affa1cbbb45aa9f9ef10b3812d358
nyg <nyg@users.noreply.github.com>
2021-05-06 22:06:32 +0200

    Update index.js to use utils and YieldTooltip

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,15 @@
 import useSWR from 'swr'
 import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 import Layout from '../components/layout'
+import YieldTooltip from '../components/yield-tooltip'
+import assetsInfo from '../utils/assets'
+import * as format from '../utils/format'
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
-
-const assetsInfo = {
-  USDC: { // https://www.centre.io/usdc-brand
-    color: '#2775CA'
-  },
-  CHSB: {
-    color: '#01c38d'
-  },
-  ETH: { // https://ethereum.org/en/assets/
-    color: '#575d84'
-  }
-}
-
-const formatter = new Intl.DateTimeFormat('en-GB', { month: 'short', day: 'numeric' })
-const longDateFormatter = new Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'short', day: 'numeric' })
 
 export default function SmartYields() {
 
   const { data, error } = useSWR('/api/yields', fetcher)
-
-  const CustomTooltip = ({ active, payload, label }) => {
-    return (
-      <div className="grid grid-cols-2 bg-gray-100 bg-opacity-90 border-2 border-gray-500 rounded-md p-3 text-sm">
-        <div className="col-span-2 text-center border-b-2 border-gray-400 mb-2">{longDateFormatter.format(label)}</div>
-        {payload.sort((a, b) => b.value - a.value).map(y => <>
-          <div key={y.dataKey} className="pr-3 ">{y.dataKey}</div>
-          <div key={y.dataKey + 'val'} className="text-right tabular-nums">{y.value.toFixed(2)}%</div>
-        </>)}
-      </div>
-    )
-  }
 
   let graph
   if (error) {
@@ -50,9 +26,9 @@ export default function SmartYields() {
             <Line key={asset} type="stepAfter" dataKey={asset} stroke={assetsInfo[asset].color} strokeWidth={2} dot={false} unit="%" />
           ))}
           <CartesianGrid stroke="#ddd" strokeDasharray="3 3" />
-          <XAxis tickMargin={10} dataKey="date" scale="time" type="number" ticks={data.xTicks} domain={['auto', 'auto']} tickFormatter={(timestamp) => formatter.format(timestamp)} />
+          <XAxis tickMargin={10} dataKey="date" scale="time" type="number" ticks={data.xTicks} domain={['auto', 'auto']} tickFormatter={(timestamp) => format.asShortDate(timestamp)} />
           <YAxis tickMargin={10} unit="%" />
-          <Tooltip content={<CustomTooltip />} />
+          <Tooltip content={<YieldTooltip />} />
           <Legend iconType="plainline" align="center" wrapperStyle={{ paddingLeft: "52px", paddingTop: "18px" }} />
         </LineChart>
       </ResponsiveContainer>
