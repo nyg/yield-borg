73c9a0817b251d52cfc30ef82c0821b8006dc217
nyg <nyg@users.noreply.github.com>
2021-05-06 23:32:12 +0200

    Add setting to choose a different line type

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import useSWR from 'swr'
+import { useCookies } from 'react-cookie'
 import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 import Layout from '../components/layout'
 import YieldTooltip from '../components/yield-tooltip'
@@ -9,8 +10,13 @@ const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function SmartYields() {
 
-  const { data, error } = useSWR('/api/yields', fetcher)
+  const [cookies, setCookie] = useCookies()
 
+  const changeLineType = event => {
+    setCookie('lineType', event.target.value)
+  }
+
+  const { data, error } = useSWR('/api/yields', fetcher)
   let graph
   if (error) {
     graph = <div className="text-center pt-4">Failed to load data!</div>
@@ -21,9 +27,9 @@ export default function SmartYields() {
   else {
     graph = (
       <ResponsiveContainer width="100%" aspect={1.618}>
-        <LineChart data={data.yields} margin={{ top: 40, right: 60, bottom: 20, left: 0 }}>
+        <LineChart data={data.yields} margin={{ top: 25, right: 60, bottom: 20, left: 0 }}>
           {data.assets.map(asset => (
-            <Line key={asset} type="stepAfter" dataKey={asset} stroke={assetsInfo[asset].color} strokeWidth={2} dot={false} unit="%" />
+            <Line key={asset} dataKey={asset} type={cookies.lineType} stroke={assetsInfo[asset].color} strokeWidth={2} dot={false} unit="%" />
           ))}
           <CartesianGrid stroke="#ddd" strokeDasharray="3 3" />
           <XAxis tickMargin={10} dataKey="date" scale="time" type="number" ticks={data.xTicks} domain={['auto', 'auto']} tickFormatter={(timestamp) => format.asShortDate(timestamp)} />
@@ -37,6 +43,15 @@ export default function SmartYields() {
 
   return (
     <Layout name="Smart Yields">
+      <div className="pl-16 mt-4 space-x-3">
+        <label htmlFor="line-type">Line type</label>
+        <select id="line-type" value={cookies.lineType} onChange={changeLineType}>
+          <option value="monotone">Monotone</option>
+          <option value="linear">Linear</option>
+          <option value="step">Step</option>
+          <option value="stepAfter">Step After</option>
+        </select>
+      </div>
       {graph}
       <div className="pl-16 pt-4">
         <ul className="list-disc">
