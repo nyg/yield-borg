9ccd49f141f411a755190c061fa29fcb4044e134
nyg <nyg@users.noreply.github.com>
2021-10-03 13:55:36 +0200

    Update import-db script to warn before dropping db…

--- db/import-db.js
+++ db/import-db.js
@@ -1,11 +1,20 @@
 const fs = require('fs').promises
-const Redis = require('ioredis');
+const Redis = require('ioredis')
+const prompt = require('prompt-sync')();
 
 (async () => {
 
   // get redis url
   const data = await fs.readFile('.env.development.local', 'ascii')
   const redisUrl = data.split('\n').filter(l => l.match(/^REDIS_URL=/))[0].split('=')[1]
+
+  // avoid erasing prod db…
+  console.warn(`You're going to drop database ${redisUrl} and import export.json into it.`)
+  const answer = prompt('Are you sure? (yes/N) ')
+  if (answer !== 'yes') {
+    console.log('Exiting…')
+    process.exit(1)
+  }
 
   // read yields and community indices from file
   const { yields, communityIndices } = JSON.parse(await fs.readFile('db/export.json'))
