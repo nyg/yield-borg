c8382b63f5658685a71014f3e30eb2c7c3cb3f25
nyg <nyg@users.noreply.github.com>
2021-10-27 00:10:13 +0200

    Add EURT and Sept report

--- pages/index.js
+++ pages/index.js
@@ -76,14 +76,15 @@ export default function SmartYields() {
       <div className="ml-16 mr-16 space-y-4">
         <div>
           <span className="font-bold mr-4">Announcements</span>
-          <a href="https://swissborg.com/blog/swissborg-launches-usdc-smart-yield">USDC</a> (Dec 14, 2020) •{' '}
-          <a href="https://swissborg.com/blog/chsb-smart-yield">CHSB</a> (Jan 29, 2021) •{' '}
-          <a href="https://swissborg.com/blog/eth-smart-yield">ETH</a> (Mar 19, 2021) •{' '}
-          <a href="https://swissborg.com/blog/btc-smart-yield">BTC</a> (May 6, 2021) •{' '}
-          <a href="https://swissborg.com/blog/bnb-smart-yield">BNB</a> (June 4, 2021) •{' '}
-          <a href="https://swissborg.com/blog/usdt-smart-yield">USDT</a> (June 25, 2021) •{' '}
-          <a href="https://swissborg.com/blog/xrp-smart-yield">XRP</a> (July 2, 2021) •{' '}
-          <a href="https://swissborg.com/blog/polygon-smart-yield">MATIC</a> (Sept 2, 2021)
+          <a href="https://swissborg.com/blog/swissborg-launches-usdc-smart-yield">USDC</a> <small>Dec 14, 2020</small> •{' '}
+          <a href="https://swissborg.com/blog/chsb-smart-yield">CHSB</a> <small>Jan 29, 2021</small> •{' '}
+          <a href="https://swissborg.com/blog/eth-smart-yield">ETH</a> <small>Mar 19, 2021</small> •{' '}
+          <a href="https://swissborg.com/blog/btc-smart-yield">BTC</a> <small>May 6, 2021</small> •{' '}
+          <a href="https://swissborg.com/blog/bnb-smart-yield">BNB</a> <small>June 4, 2021</small> •{' '}
+          <a href="https://swissborg.com/blog/usdt-smart-yield">USDT</a> <small>June 25, 2021</small> •{' '}
+          <a href="https://swissborg.com/blog/xrp-smart-yield">XRP</a> <small>July 2, 2021</small> •{' '}
+          <a href="https://swissborg.com/blog/polygon-smart-yield">MATIC</a> <small>Sept 2, 2021</small> •{' '}
+          <a href="https://swissborg.com/blog/polygon-smart-yield">EURT</a> <small>Oct, 2021</small>
         </div>
         <div>
           <span className="font-bold mr-4">Smart Yield Reports</span>
@@ -94,7 +95,8 @@ export default function SmartYields() {
           <a href="https://swissborg.com/blog/smart-yield-report-may-2021">May 21</a> •{' '}
           <a href="https://swissborg.com/blog/smart-yield-report-june-2021">June 21</a> •{' '}
           <a href="https://swissborg.com/blog/smart-yield-report-july-2021">July 21</a> •{' '}
-          <a href="https://swissborg.com/blog/smart-yield-report-august-2021">Aug 21</a>
+          <a href="https://swissborg.com/blog/smart-yield-report-august-2021">Aug 21</a> •{' '}
+          <a href="https://swissborg.com/blog/smart-yield-report-september-2021">Sept 21</a>
         </div>
       </div>
     </Layout>
