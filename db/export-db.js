052432d8233a4edb3e805b8c59e7c45aed4782f0
nyg <nyg@users.noreply.github.com>
2021-10-28 21:12:03 +0200

    Update db scripts

--- db/export-db.js
+++ db/export-db.js
@@ -1,21 +1,24 @@
-const fs = require('fs').promises
+const fs = require('fs')
 const Redis = require('ioredis');
 
 (async () => {
 
   // get redis url
-  const data = await fs.readFile('.env.development.local', 'ascii')
+  const data = fs.readFileSync('.env.development.local', 'ascii')
   const redisUrl = data.split('\n').filter(l => l.match(/^REDIS_URL=/))[0].split('=')[1]
 
-  // get all yields and community indices
+  // get yields, yield averages and community indices
   const redis = new Redis(redisUrl)
   const yields = await redis.lrange('yields', 0, -1)
+  const averages = await redis.lrange('averages', 0, -1)
   const communityIndices = await redis.lrange('communityIndices', 0, -1)
   redis.quit()
 
   // save yields to file
-  await fs.writeFile('db/export.json', JSON.stringify({
-    yields: yields, communityIndices: communityIndices
+  fs.writeFileSync('db/data.json', JSON.stringify({
+    yields,
+    averages,
+    communityIndices
   }, null, '\t'))
 
   console.log('Export done.')
--- db/import-db.js
+++ db/import-db.js
@@ -1,15 +1,15 @@
-const fs = require('fs').promises
+const fs = require('fs')
 const Redis = require('ioredis')
 const prompt = require('prompt-sync')();
 
 (async () => {
 
   // get redis url
-  const data = await fs.readFile('.env.development.local', 'ascii')
+  const data = fs.readFileSync('.env.development.local', 'ascii')
   const redisUrl = data.split('\n').filter(l => l.match(/^REDIS_URL=/))[0].split('=')[1]
 
   // avoid erasing prod db…
-  console.warn(`You're going to drop database ${redisUrl} and import export.json into it.`)
+  console.warn(`You're going to drop database ${redisUrl} and import data.json into it.`)
   const answer = prompt('Are you sure? (yes/N) ')
   if (answer !== 'yes') {
     console.log('Exiting…')
@@ -17,13 +17,16 @@ const prompt = require('prompt-sync')();
   }
 
   // read yields and community indices from file
-  const { yields, communityIndices } = JSON.parse(await fs.readFile('db/export.json'))
+  const { yields, averages, communityIndices } = JSON.parse(await fs.readFile('db/data.json'))
 
   // import values into db
   const redis = new Redis(redisUrl)
 
   await redis.del('yields')
   await redis.rpush('yields', yields)
+
+  await redis.del('averages')
+  await redis.rpush('averages', averages)
 
   await redis.del('communityIndices')
   await redis.rpush('communityIndices', communityIndices)
