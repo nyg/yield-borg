8445f6ed7d00160f144d61ae454881e8cc19a9f8
nyg <nyg@users.noreply.github.com>
2021-10-29 18:41:09 +0200

    Minor change

--- components/shared/active-link.js
+++ components/shared/active-link.js
@@ -5,12 +5,12 @@ export default function ActiveLink({ href, children }) {
 
   const { asPath } = useRouter()
   const style = asPath === href
-    ? 'bg-gray-500 p-1 rounded-lg text-white hover:text-gray-50 no-underline w-full'
-    : 'bg-gray-100 p-1 rounded-lg text-gray-600 hover:text-gray-700 no-underline w-full'
+    ? 'bg-gray-500 text-white hover:text-gray-50 '
+    : 'bg-gray-100 text-gray-600 hover:text-gray-700'
 
   return (
     <Link href={href} >
-      <a className={style}>
+      <a className={`${style} p-1 rounded-lg no-underline w-full`}>
         {children}
       </a>
     </Link>
