ac622bd7816f764e545cbd8af62fb9a44c79cb8c
nyg <nyg@users.noreply.github.com>
2021-10-31 18:14:36 +0100

    Name default function

--- pages/api/cron/_middleware.js
+++ pages/api/cron/_middleware.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server"
 
-export default (req, event) =>
-  req.headers.get('authorization') === `Bearer ${process.env.CRON_KEY}`
+export default function authMiddleware(req) {
+  return req.headers.get('authorization') === `Bearer ${process.env.CRON_KEY}`
     ? NextResponse.next()
     : new NextResponse('Authorization required!', { status: 403 })
+}
