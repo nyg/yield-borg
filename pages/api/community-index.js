21bd1e5d70b0d5fe3e06782833be65849667ee3a
nyg <nyg@users.noreply.github.com>
2024-10-27 21:37:56 +0100

    Switch community indices to PostgreSQL (#123)

--- pages/api/community-index.js
+++ pages/api/community-index.js
@@ -1,14 +1,13 @@
-import redis from '../../db/redis'
+import { pgSql } from '../../db/redis'
 
 
 export default async function getCommunityIndex(req, res) {
 
-  const communityIndices = (await redis.lrange('communityIndices', 0, -1))
-    .map(communityIndexString => {
-      const communityIndex = JSON.parse(communityIndexString)
-      communityIndex.date = new Date(communityIndex.date).getTime()
-      return communityIndex
-    })
+  const communityIndices = (await pgSql`select * from community_indices`)
+    .map(e => ({
+      date: e.date.getTime(),
+      value: e.value,
+    }))
 
   res.status(200).json({ communityIndices })
 }
