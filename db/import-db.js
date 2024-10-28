d120414a805af2fa4943bd3d6b4a510076006668
nyg <nyg@users.noreply.github.com>
2024-10-28 20:49:55 +0100

    Fix export-db script

--- db/import-db.js
+++ db/import-db.js
@@ -4,34 +4,34 @@ const prompt = require('prompt-sync')();
 
 (async () => {
 
-  // get redis url
-  const data = fs.readFileSync('.env.development.local', 'ascii')
-  const redisUrl = data.split('\n').filter(l => l.match(/^REDIS_URL=/))[0].split('=')[1]
+   // get redis url
+   const data = fs.readFileSync('.env.development.local', 'ascii')
+   const redisUrl = data.split('\n').filter(l => l.match(/^REDIS_URL=/))[0].split('=')[1]
 
-  // avoid erasing prod db…
-  console.warn(`You're going to drop database ${redisUrl} and import data.json into it.`)
-  const answer = prompt('Are you sure? (yes/N) ')
-  if (answer !== 'yes') {
-    console.log('Exiting…')
-    process.exit(1)
-  }
+   // avoid erasing prod db…
+   console.warn(`You're going to drop database ${redisUrl} and import data.json into it.`)
+   const answer = prompt('Are you sure? (yes/N) ')
+   if (answer !== 'yes') {
+      console.log('Exiting…')
+      process.exit(1)
+   }
 
-  // read yields and community indices from file
-  const { yields, averages, communityIndices } = JSON.parse(fs.readFileSync('db/data.json'))
+   // read yields and community indices from file
+   const { yields, averages, communityIndices } = JSON.parse(fs.readFileSync('db/data.json'))
 
-  // import values into db
-  const redis = new Redis(redisUrl)
+   // import values into db
+   const redis = new Redis(redisUrl)
 
-  await redis.del('yields')
-  await redis.rpush('yields', yields)
+   await redis.del('yields')
+   await redis.rpush('yields', yields)
 
-  await redis.del('averages')
-  await redis.rpush('averages', averages)
+   await redis.del('averages')
+   await redis.rpush('averages', averages)
 
-  await redis.del('communityIndices')
-  await redis.rpush('communityIndices', communityIndices)
+   await redis.del('communityIndices')
+   await redis.rpush('communityIndices', communityIndices)
 
-  redis.quit()
+   redis.quit()
 
-  console.log('Import done.')
+   console.log('Import done.')
 })()
