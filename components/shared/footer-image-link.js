f96307f45adbd840561390075e124982a6435ed0
nyg <nyg@users.noreply.github.com>
2025-03-15 23:17:23 +0100

    Misc changes (#141)

--- components/shared/footer-image-link.js
+++ components/shared/footer-image-link.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function FooterImageLink({ href, src, alt }) {
 
    return (
-      <a href={href} className="inline-block m-0 p-0 leading-3">
+      <a href={href} className="inline-block m-0 p-0 pl-2 pr-2 leading-3">
          <Image src={src} alt={alt} width="18" height="18" />
       </a>
    )
